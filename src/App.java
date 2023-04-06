c0c93e9bf1eeabcf4a9dce2c9cec98b592249638
Mateus Saullo <mateusaullo13@gmail.com>
2023-04-05 22:50:58 -0300

    primeiro commit

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        double hora,mes,salario,ir,inss,Sindicato,liquido;
+        double hora,mes,salario,ir,inss,sindicato,liquido;
         
         Scanner teclado = new Scanner(System.in);
 
@@ -15,13 +15,13 @@ public class App {
           salario = hora * mes;
           ir = (salario *11 / 100);
           inss = (salario * 8 / 100);
-          Sindicato = (salario * 5 / 100);
-          liquido = salario - ir - inss - Sindicato;
+          sindicato = (salario * 5 / 100);
+          liquido = salario - ir - inss - sindicato;
 
           System.out.printf("\n- Salário Bruto: R$%.2f\n", salario);
           System.out.printf("- IR (11%%) : R$%.2f\n", ir);
           System.out.printf("- INSS (8%%) : R$%.2f\n", inss);
-          System.out.printf("- Sindicato (5%%) : R$%.2f\n", Sindicato);
+          System.out.printf("- Sindicato (5%%) : R$%.2f\n", sindicato);
           System.out.printf("- Salário Liquido : R$%.2f\n",liquido);
     }
 }
