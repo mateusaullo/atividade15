3fb8d6bca3331dec8bee217f1bc06399ce30d4e2
Mateus Saullo <mateusaullo13@gmail.com>
2023-04-05 22:49:49 -0300

    primeiro commit

--- src/App.java
+++ src/App.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        double hora,mes,salario,IR,INSS,Sindicato,liquido;
+        double hora,mes,salario,ir,inss,Sindicato,liquido;
         
-        
-
         Scanner teclado = new Scanner(System.in);
 
          System.out.print("Quanto voce ganha por hora?:");
@@ -15,14 +13,14 @@ public class App {
           teclado.close();
 
           salario = hora * mes;
-          IR = (salario *11 / 100);
-          INSS = (salario * 8 / 100);
+          ir = (salario *11 / 100);
+          inss = (salario * 8 / 100);
           Sindicato = (salario * 5 / 100);
-          liquido = salario - IR - INSS - Sindicato;
+          liquido = salario - ir - inss - Sindicato;
 
           System.out.printf("\n- Salário Bruto: R$%.2f\n", salario);
-          System.out.printf("- IR (11%%) : R$%.2f\n", IR);
-          System.out.printf("- INSS (8%%) : R$%.2f\n", INSS);
+          System.out.printf("- IR (11%%) : R$%.2f\n", ir);
+          System.out.printf("- INSS (8%%) : R$%.2f\n", inss);
           System.out.printf("- Sindicato (5%%) : R$%.2f\n", Sindicato);
           System.out.printf("- Salário Liquido : R$%.2f\n",liquido);
     }
